aa6551ae7541827b0c41ad2d4c132c27fcb3fe9d
Robby Nevels <rjnevels@fb.com>
2015-11-23 23:58:21 -0600

    update process variance of second example

--- src/example2.js
+++ src/example2.js
@@ -3,27 +3,36 @@
  * state include a position and velocity, only the position is being measured,
  * and the velocity is assumed to be constant.
  *
- * In this senario, a running average would be just as effective as  Kalman
- * filter.
+ * Since the velocity is constant, it's probably just as efficient to use a
+ * running average or linear regression to determine the state.
  *
  * @flow
  */
 
 const KalmanFilter = require('./KalmanFilter');
-const {abs, index, matrix, multiply, pow, subset, transpose} = require('mathjs');
+const {index, matrix, multiply, pow, subset, ones} = require('mathjs');
 const {makeCanvasFitWindow, normal} = require('./utils');
 
 const DT = 1;
 const START = 1.3;
 const STEPS = 50;
-const MEASUREMENT_VARIANCE = .2;
+const MEASUREMENT_VARIANCE = .4;
 const VELOCITY = .1;
-const PROCESS_VARIANCE = .0001;
 
-const ACCELERATION = matrix([
-  [.5 * DT * DT],
-  [DT],
-]);
+/*
+ * Some examples I've found use the next derivative of a state to estimate the
+ * process variance, to simulate unknown forces. In this case, that would be
+ * acceleration, since we're already estimating position and velocity.
+ *
+ * Seems to me that if the force is truly unknown and random, it doesn't much
+ * matter what the process variance is, so long it increases as DT increases.
+ * (Since the longer you go without measuring, the more uncertain you should
+ * be with your state estimation)
+ *
+ * In our simulation here, the state model precisely reflects the process, so
+ * this value can just be zero.
+ */
+const PROCESS_VARIANCE = 0;
 
 // hack to get value out 1x1 mathjs matrix
 function getScalar(m: Object, row: number, col: number) {
@@ -53,14 +62,14 @@ function example(): number {
       [0],
     ]),
     MeasurementCovariance: matrix([
-      [.2],
+      [MEASUREMENT_VARIANCE],
     ]),
     ObservationModel: matrix([
       [1, 0],
     ]),
     ProcessCovariance: multiply(
-      pow(PROCESS_VARIANCE, 2),
-      multiply(ACCELERATION, transpose(ACCELERATION))
+      PROCESS_VARIANCE,
+      ones(2, 2)
     ),
     StateTransitionModel: matrix([
       [1, DT],
@@ -96,12 +105,8 @@ function example(): number {
 
   let totalError  = 0;
   measurementData.forEach((_, i) => {
-    //console.log(measurementData[i], stateData[i], covarianceData[i]);
-    if (i === 0) return;
-    totalError += abs(stateData[i][0] - trueData[i]);
+    console.log(measurementData[i], stateData[i], covarianceData[i]);
   });
-  //return totalError;
-
 
   /*
    * Draw the data
